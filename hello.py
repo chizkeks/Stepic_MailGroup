48f38ec75a8032a82ad8698bd8cf527e50ed5a74
Ilya Pavlov <ilya_pavlov@yahoo.com>
2017-04-14 16:18:19 +0300

    second assignment

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def wsgi_application (environ, start_response):
+def app (environ, start_response):
     body = [bytes (i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return body
