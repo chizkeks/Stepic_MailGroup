7445c7bfa0862ce2e636e3aa0baa8b8da7c7d383
Ilya Pavlov <ilya_pavlov@yahoo.com>
2017-06-22 12:43:50 +0300

    Sending taks 2.1

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
