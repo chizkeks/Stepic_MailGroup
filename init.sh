bc90b26e78fbf27c0f35c79c9a5619d1ccdc2940
Ilya Pavlov <ilya_pavlov@yahoo.com>
2017-06-25 19:04:35 +0300

    Part2

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask:wsgi_application
+sudo gunicorn --bind=0.0.0.0:8000 ask.wsgi:application &
