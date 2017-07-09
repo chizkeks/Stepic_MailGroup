f9118e74400d227e98d8e8cd79b94b7f748afc52
Ilya Pavlov <ilya_pavlov@yahoo.com>
2017-07-09 21:00:26 +0300

    sending task for 2.5 module

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,16 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from ask.qa import views
+from . import views
 
+from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('qa.urls')),
-    url(r'^login/.*$', include('qa.urls')),
-    url(r'^signup/.*$', include('qa.urls')),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.question_text, name='question'),
-    url(r'^ask/.*$', include('qa.urls')),
-    url(r'^popular/.*$', views.popular_questions),
-    url(r'^new/.*$', views.new_questions),
-    url(r'^admin/$', admin.site.urls),
+    url(r'^', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
