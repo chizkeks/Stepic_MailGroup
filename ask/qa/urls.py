7418c77afa1e7d35caee128b11c10567103d9499
Ilya Pavlov <ilya_pavlov@yahoo.com>
2017-07-09 21:58:06 +0300

    sending task for 2.5 module

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test),
-    url(r'^login/.*$', include('qa.urls')),
-    url(r'^signup/.*$', include('qa.urls')),
+    url(r'^login/.*$', views.test),
+    url(r'^signup/.*$', views.test),
     url(r'^question/(?P<question_id>[0-9]+)/$', views.question_text, name='question'),
-    url(r'^ask/.*$', include('qa.urls')),
+    url(r'^ask/.*$', views.test),
     url(r'^popular/.*$', views.popular_questions),
     url(r'^new/.*$', views.new_questions),
 ]
